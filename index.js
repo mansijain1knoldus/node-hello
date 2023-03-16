53de2f8e2dd91e22927dac0d036e14aad6156938
mansijain1knoldus <126751871+mansijain1knoldus@users.noreply.github.com>
2023-03-16 18:36:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Mansi!!! Project is running \n'
+  const msg = 'Hello Mansi!!! This is node \n'
   res.end(msg);
 });
 
