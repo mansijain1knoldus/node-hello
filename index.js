45388b58f91c6be63c2c764468df16e3e392f6df
mansijain1knoldus <126751871+mansijain1knoldus@users.noreply.github.com>
2023-03-16 18:31:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Mansi!!! This is autocommited line \n'
+  const msg = 'Hello Mansi!!! Project is running \n'
   res.end(msg);
 });
 
