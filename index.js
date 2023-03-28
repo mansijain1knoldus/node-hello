3aa1410df1235063f31412ce42aabcf56b00a12a
mansijain1knoldus <126751871+mansijain1knoldus@users.noreply.github.com>
2023-03-28 16:18:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Mansi!!! This is main Node  \n'
+  const msg = 'Hello Mansi!!! This is DEV Node  \n'
   res.end(msg);
 });
