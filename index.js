c96f536fd90cdeeb9c2f8934cb056ca2d1e021de
mansijain1knoldus <126751871+mansijain1knoldus@users.noreply.github.com>
2023-03-28 16:18:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Mansi!!! This is Node  \n'
+  const msg = 'Hello Mansi!!! This is main Node  \n'
   res.end(msg);
 });
-
-const server2 = http.createServer((req, res) => {
-	res.statusCode = 200;
-	const msg2 = 'Hello Mansi.. This is Node!\n'
-	res.end(msg2);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
-
-server2.listen(port2, () => {
-	console.log(`Server2 running on http://localhost:${port2}/`);
-})
